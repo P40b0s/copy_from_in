1930288034580d191b6437ec47f6ea8482d66f60
desmond0002 <grazyjack83@gmail.com>
2024-02-08 11:00:22 +0300

    front branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,37 @@
 import "./App.scss";
+import { useState } from "react";
 
 function App() {
+  const [selectedId, setSelectedId] = useState<number>(1);
   return (
     <>
-      <div className="container">
-        <div className="tabs"></div>
-        <div className="content">
-          <p>Content</p>
+      <div className="tabs">
+        <div className="tabs__row">
+          <div
+            className={`tab ${selectedId == 1 ? "selected" : ""}`}
+            onClick={() => setSelectedId(1)}
+          >
+            Статус
+          </div>
+          <div
+            className={`tab ${selectedId == 2 ? "selected" : ""}`}
+            onClick={() => setSelectedId(2)}
+          >
+            Ошибки
+          </div>
+          <div
+            className={`tab ${selectedId == 3 ? "selected" : ""}`}
+            onClick={() => setSelectedId(3)}
+          >
+            Сервер
+          </div>
         </div>
+      </div>
+      <div className="textarea">
+        <p>It was a dark and stormy night...1</p>
+        <p>It was a dark and stormy night...2</p>
+
+        <p>It was a dark and stormy night...3</p>
       </div>
     </>
   );
